938a100acd1a24e18a2ea173d9a67b9d377e9b2b
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-08-30 20:20:44 +0200

    codepen

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Hello. Hola. Bonjour. ようこそ！</h1>
     <p>My name is Adam Wier. I'm an American living in France. I will be quitting my job of teaching English to go into web development at the end of September. This hackathon seemed like a good way to practice and meet people!</p>
     <p>Bonjour. Je m'appelle Adam Wier. Je suis américain et j'habite en France. Je vais quitter mon travail comme enseignant d'anglais pour entrer dans le domaine du développement web. Ce hackthon me semblait une bonne manière de m'entraîner et rencontre des gens!</p>
+    <p>See my CodePen with my FreeCodeCamp projects at <a href="https://codepen.io/donthatedontkill/">https://codepen.io/donthatedontkill/</a></p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
